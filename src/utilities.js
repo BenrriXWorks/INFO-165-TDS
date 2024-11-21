8009cff175794363835ea634ad37c360bd13dd8a
BenrriXWorks <BenrriXWorks@gmail.com>
2024-11-21 02:31:42 -0300

    Comando 'help' implementado

--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process')
+const chalk = require('chalk')
 
 function getSafe(fn, ...args) {
     try {
@@ -22,6 +23,36 @@ function compile_jison(grammarPath, lexerPath) {
     return err ?? "Ok"
 }
 
+function getHelp(input, help_messages){
+    let tokens = input.split(/\s+/)
+    tokens.shift()
+    if (tokens.length == 0){ 
+        Object.keys(help_messages)
+            .forEach(command => {
+                const properties = help_messages[command]
+                console.log(`    | ${ chalk.blue(command) }${ chalk.green(properties["Parametros"] ?? "") }: ${ properties['Descripción corta'] }`)})
+        return 'Ok'
+    } 
+
+    tokens.forEach(token => {
+        const command = help_messages[token]
+        if (command != undefined) 
+            console.log(
+                `     COMANDO : ${chalk.green(token)}\n`,
+                `    Tipo    : ${command['Tipo']}\n`,
+                `    Descripcion: ${command['Descripción larga']}\n`,
+                `    Ejemplos: \n`,
+                command['Ejemplos'].map(ex=>JSON.stringify(ex, undefined, 8).replace(/^\{|\}$/g, '')).join('')
+            )
+        else 
+            console.log(`[${chalk.red(token)}]: Instrucción no definida...`)
+    })
+    return 'Ok'
+
+}
+
+
 exports.compile_jison = compile_jison
 exports.getSafe = getSafe
 exports.getSafeAsync = getSafeAsync
+exports.getHelp = getHelp
